dedfd35a7f4612fc145d1aa1b25d85dde2b7595b
SRparadox <shazer.rizzo@gmail.com>
2024-11-21 16:10:00 -0800

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -14,4 +14,52 @@ const canvas = document.createElement("canvas"); canvas.width = 256; canvas.heig
 canvas.id = "myCanvas";
 app.appendChild(canvas);
 
+const context = canvas.getContext("2d");
 
+// event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+canvas.addEventListener("mousedown", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isDrawing) {
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+  }
+});
+
+window.addEventListener("mouseup", (e) => {
+    if (isDrawing) {
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+  }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+
+//Clear Button
+const Clear = document.createElement("button"); 
+Clear.innerHTML = "Clear";
+Clear.addEventListener('click', () => {
+    console.log('Button clicked!');
+    context.clearRect(0, 0, canvas.width, canvas.height);  //Taking inspiration from: https://www.squash.io/tutorial-utilizing-the-mouseeventhandlers-in-typescript/
+});
+app.appendChild(Clear);
